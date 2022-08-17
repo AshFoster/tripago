884267074bc4be95b20a823d99001489a6c1b02f
AshFoster <ashfoster87@gmail.com>
2022-08-17 06:57:11 +0000

    Add custom fetch hook

---
+++ tripago/src/components/TripList.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { useFetch } from '../hooks/useFetch'
+
+// Styles
+import './TripList.css'
+
+export default function TripList() {
+    const [url, setUrl] = useState('https://3000-ashfoster-tripago-s91dio11d2c.ws-eu61.gitpod.io/trips')
+    const { data: trips } = useFetch(url)
+        
+    return (
+        <div className="trip-list">
+            <h2>Trip List</h2>
+            <ul>
+                {trips && trips.map(trip => (
+                    <li key={trip.id}>
+                        <h3>{trip.title}</h3>
+                        <p>{trip.price}</p>
+                    </li>
+                ))}
+            </ul>
+            <div className="filters">
+                <button onClick={() => setUrl('https://3000-ashfoster-tripago-s91dio11d2c.ws-eu61.gitpod.io/trips?loc=europe')}>
+                    European Trips
+                </button>
+                <button onClick={() => setUrl('https://3000-ashfoster-tripago-s91dio11d2c.ws-eu61.gitpod.io/trips')}>
+                    All Trips
+                </button>
+            </div>
+        </div>
+    )
+}
